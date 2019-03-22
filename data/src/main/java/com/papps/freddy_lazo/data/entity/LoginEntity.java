c038489b4ad86ff329b6c11acdb4da43bfe07c03
Freddy <flazo@peuapps.com.pe>
2019-03-21 22:13:31 -0500

    avance de casa

---
+++ data/src/main/java/com/papps/freddy_lazo/data/entity/LoginEntity.java
@@ -0,0 +1,4 @@
+package com.papps.freddy_lazo.data.entity;
+
+public class LoginEntity {
+}
--- data/src/main/java/com/papps/freddy_lazo/data/network/RestApiImpl.java
+++ data/src/main/java/com/papps/freddy_lazo/data/network/RestApiImpl.java
@@ -4,6 +4,11 @@ import android.content.Context;
 
 import com.papps.freddy_lazo.data.network.network.RestService;
 
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import retrofit2.Call;
+import retrofit2.Response;
+
 public class RestApiImpl implements RestApi {
 
 
@@ -13,5 +18,21 @@ public class RestApiImpl implements RestApi {
     public RestApiImpl(Context context, RestService restService) {
         this.context = context;
         this.restService = restService;
+    }
+
+
+    @Override
+    public Observable<ValidateQrEntity> login(long customerUserId, String code) {
+        return Observable.create(emitter -> restService.validateQrCode(customerUserId, new BodyValidateQrCode(code)).enqueue(new DefaultCallback<ResponseEntity<QrValidateResponse>>(emitter) {
+            @Override
+            public void onResponse(@NonNull Call<ResponseEntity<QrValidateResponse>> call, @NonNull Response<ResponseEntity<QrValidateResponse>> response) {
+                super.onResponse(call, response);
+                ResponseEntity<QrValidateResponse> body = response.body();
+                if (body != null && body.getError() == null) {
+                    emitter.onNext(body.getBody().getValidateQrEntity());
+                    emitter.onComplete();
+                }
+            }
+        }));
     }
 }
