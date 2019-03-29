55348821f4a5b3dbb6fb69670de3908091ac891f
Freddy <flazo@peuapps.com.pe>
2019-03-29 00:20:52 -0500

    camara y galeria

---
+++ presentation/src/main/java/com/papps/freddy_lazo/redvet/RedVetGlideModule.java
@@ -0,0 +1,8 @@
+package com.papps.freddy_lazo.redvet;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class RedVetGlideModule extends AppGlideModule {
+}
