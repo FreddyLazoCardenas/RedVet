eef7bfa19e31a5f595375a476d4166236e7b2580
Freddy <flazo@peuapps.com.pe>
2019-04-06 15:44:07 -0500

    se configura el fcm

---
+++ presentation/src/doctor/java/com/papps/freddy_lazo/redvet/view/service/FirebaseMessageService.java
@@ -0,0 +1,12 @@
+package com.papps.freddy_lazo.redvet.view.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessageService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+    }
+}
