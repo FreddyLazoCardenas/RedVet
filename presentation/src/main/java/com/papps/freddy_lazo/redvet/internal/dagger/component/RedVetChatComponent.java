7c1fbb95f89cb57b15e7eb64f902c39f59b5c845
Freddy Lazo <flazocar@everis.com>
2019-04-12 10:06:42 -0500

    chat en doctor

--- presentation/src/main/java/com/papps/freddy_lazo/redvet/internal/dagger/component/RedVetChatComponent.java
+++ presentation/src/main/java/com/papps/freddy_lazo/redvet/internal/dagger/component/RedVetChatComponent.java
@@ -3,7 +3,6 @@ package com.papps.freddy_lazo.redvet.internal.dagger.component;
 
 import com.papps.freddy_lazo.redvet.internal.dagger.PerActivity;
 import com.papps.freddy_lazo.redvet.internal.dagger.module.ActivityModule;
-import com.papps.freddy_lazo.redvet.view.activity.CancelAppointmentActivity;
 import com.papps.freddy_lazo.redvet.view.activity.ChatActivity;
 
 import dagger.Component;
