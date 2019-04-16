057eb87271ae0f817d700f2c43017bae9dee148a
Freddy Lazo <flazocar@everis.com>
2019-04-15 22:51:47 -0500

    se avanza con notificaciones

--- presentation/src/doctor/java/com/papps/freddy_lazo/redvet/view/service/FirebaseMessageService.java
+++ presentation/src/doctor/java/com/papps/freddy_lazo/redvet/view/service/FirebaseMessageService.java
@@ -5,10 +5,13 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class FirebaseMessageService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Map<String, String> data = remoteMessage.getData();
         Log.d("remoteMessage",remoteMessage.toString());
     }
 }
