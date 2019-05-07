b9549c856f31cc95c6ed3c499a212cc99bd356f5
Freddy <flazo@peuapps.com.pe>
2019-05-07 01:20:18 -0500

    se agrega rating

--- presentation/src/main/java/com/papps/freddy_lazo/redvet/model/mapper/DoctorModelMapper.java
+++ presentation/src/main/java/com/papps/freddy_lazo/redvet/model/mapper/DoctorModelMapper.java
@@ -22,7 +22,7 @@ public class DoctorModelMapper {
     }
 
     public static DoctorModel transform(Doctor doctor) {
-        return new DoctorModel(doctor.getUser_id(), doctor.getType_document(), doctor.getNumber_document(), doctor.getBusiness_name(), doctor.getAddress(), doctor.getLatitude(), doctor.getLongitude(), doctor.getConsultation_price(), doctor.getConsultation_time(), doctor.getShower_price(), doctor.getShower_time(), doctor.getPhone(), doctor.getType(), doctor.getTuition_number()
+        return new DoctorModel(doctor.getRate(), doctor.getUser_id(), doctor.getType_document(), doctor.getNumber_document(), doctor.getBusiness_name(), doctor.getAddress(), doctor.getLatitude(), doctor.getLongitude(), doctor.getConsultation_price(), doctor.getConsultation_time(), doctor.getShower_price(), doctor.getShower_time(), doctor.getPhone(), doctor.getType(), doctor.getTuition_number()
                 , doctor.getDescription(), doctor.getAttention(), doctor.getAvailable(), doctor.getFirst_name(), doctor.getLast_name(), doctor.getEmail(), doctor.getPhoto_url(), doctor.getApi_token(), PetModelMapper.transform(doctor.getPetList()), ScheduleMapper.transform(doctor.getScheduleList()), ServiceDoctorMapper.transform(doctor.getServiceList()));
     }
 }
