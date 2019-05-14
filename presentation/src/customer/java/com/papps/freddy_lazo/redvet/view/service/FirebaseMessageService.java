0cc075faf09b6579f1902ce3533863e4bb1eb08a
Freddy Lazo <flazocar@everis.com>
2019-05-14 11:03:17 -0500

    s

--- presentation/src/customer/java/com/papps/freddy_lazo/redvet/view/service/FirebaseMessageService.java
+++ presentation/src/customer/java/com/papps/freddy_lazo/redvet/view/service/FirebaseMessageService.java
@@ -12,6 +12,7 @@ import com.papps.freddy_lazo.domain.model.Notification;
 import com.papps.freddy_lazo.redvet.AndroidApplication;
 import com.papps.freddy_lazo.redvet.internal.bus.Event;
 import com.papps.freddy_lazo.redvet.internal.bus.RxBus;
+import com.papps.freddy_lazo.redvet.view.activity.ChatActivity;
 import com.papps.freddy_lazo.redvet.view.activity.HomeActivity;
 import com.papps.freddy_lazo.redvet.view.activity.SplashActivity;
 import com.papps.freddy_lazo.redvet.view.util.NotificationUtil;
@@ -32,13 +33,23 @@ public class FirebaseMessageService extends FirebaseMessagingService {
         AndroidApplication mApp = (AndroidApplication) getApplication();
         Map<String, String> data = remoteMessage.getData();
         Log.d("remoteMessage", remoteMessage.toString());
-
+        boolean isFromChat = data.get("message") != null && Objects.equals(data.get("message"), "Mensaje recibido") && data.get("appointment_id") != null;
         if (mApp.isAlive()) {
-            Intent notificationIntent = new Intent(getApplicationContext(), HomeActivity.class);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mApp.isBackground() ? notificationIntent : new Intent(), 0);
-            NotificationUtil.showNotification(this, data.get("type"), data.get("message"), pendingIntent);
+            if (isFromChat) {
+                Intent notificationIntent = new Intent(getApplicationContext(), ChatActivity.class);
+                int i = Integer.valueOf(data.get("appointment_id"));
+                notificationIntent.putExtra("data", i);
+                notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,  notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationUtil.showNotification(this, data.get("type"), data.get("message"), pendingIntent);
+            } else {
+                Intent notificationIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mApp.isBackground() ? notificationIntent : new Intent(), 0);
+                NotificationUtil.showNotification(this, data.get("type"), data.get("message"), pendingIntent);
+            }
         } else {
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
                     new Intent(getApplicationContext(), SplashActivity.class), 0);
@@ -50,7 +61,7 @@ public class FirebaseMessageService extends FirebaseMessagingService {
         saveNotification.bindParams(new Notification(data.get("type"), data.get("appointment_id"), data.get("message"), timeStamp, false));
         saveNotification.execute(new SaveDataObservable());
         if (data.get("message") != null && Objects.equals(data.get("message"), "Mensaje recibido") && data.get("appointment_id") != null) {
-            rxBus.send(new Event.NotificationChatEvent(Integer.valueOf((data.get("appointment_id")))));
+            //rxBus.send(new Event.NotificationChatEvent(Integer.valueOf((data.get("appointment_id")))));
         } else if (data.get("appointment_id") != null)
             rxBus.send(new Event.NotificationEvent(Integer.valueOf((data.get("appointment_id")))));
     }
